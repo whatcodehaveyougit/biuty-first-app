50dfd855936f4023c2026cacc54a3083a69b637e
whatcodehaveyougit <siggywatt@yahoo.com>
2022-12-28 15:59:01 +0000

    modifiers tweak

--- src/routes/mixins/mixins.js
+++ src/routes/mixins/mixins.js
@@ -12,7 +12,7 @@ const Mixins = () => {
         <UITitle level='3' label='Mixins, questions' />
         <UIP modifiers='' label='Ellipsis, label, where do I find the default styles etc?' />
         <UIP modifiers='funky-text' label='This is some funky text, but how do I make it red?' />
-        <UIP modifiers='super-funky-text' label='This inherits funky text but is also super too' />
+        <UIP modifiers='super-funky-text' label='This is super funky text' />
         <p className='test-placeholder'>This is styled using a Placeholder</p>
         <div className='margin-top-1'>
           <UIButton type='submit' label='Click me' onClick={() => setButtonState(buttonState => !buttonState) } modifiers={buttonState ? 'btn btn-success' : 'btn'} />
